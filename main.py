9a932e0d0d8bea7d7b4b5ef69a2a548714399ffe
Alex <115658508+Alexlimoeses@users.noreply.github.com>
2023-04-10 13:59:00 -0300

    Criada a parte de conversão de moedas para reais

---
+++ main.py
@@ -0,0 +1,26 @@
+import requests
+import pandas as pd 
+from datetime import datetime
+import time
+
+class conversor_moeda:
+    def __init__(self):
+        self.conversao = requests.get("http://economia.awesomeapi.com.br/json/last/USD-BRL,EUR-BRL,BTC-BRL,JPY-BRL,USD-BRLT")
+        #Pega as moedas disponíveis no site (Atualizado a cada 30 segundos) e armazena
+        
+        self.requisicao = self.conversao.json()
+        
+        self.cotacao_dolar = self.requisicao["USDBRL"]["bid"]
+        self.cotacao_euro = self.requisicao["EURBRL"]["bid"]
+        self.cotacao_bitc = self.requisicao["BTCBRL"]["bid"]
+        self.cotacao_yene = self.requisicao["JPYBRL"]["bid"]
+        self.cotacao_dolartur = self.requisicao["USDBRLT"]["bid"]
+        #Armazena cada moeda em sua devida variavel
+        
+    def most(self):
+        self.doli = float(self.cotacao_dolar)
+        print('O dolar atual é',self.doli)
+        
+
+aa = conversor_moeda()
+aa.most()
