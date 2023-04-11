4702ce7e47bf3b6ad5ae3b64ec737da8f67062d9
Alex <115658508+Alexlimoeses@users.noreply.github.com>
2023-04-11 15:56:58 -0300

    Adicionada a função de grafico com alguns bugs

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import requests
+import matplotlib.pyplot as plt
+from alpha_vantage.foreignexchange import ForeignExchange
+from datetime import datetime, timedelta
+import pandas as pd
 
 class conversor_moeda:
     def __init__(self):
@@ -14,10 +18,34 @@ class conversor_moeda:
         self.cotacao_dolartur = self.requisicao["USDBRLT"]["bid"]
         #Armazena cada moeda em sua devida variavel
         
-    def most(self):
-        self.doli = float(self.cotacao_dolar)
-        print('O dolar atual é',self.doli)
+    def graff(self):
+        # Defina a chave da API da Alpha Vantage
+        self.api_key = 'SUA_CHAVE_API'
+
+        # Defina o símbolo da moeda que você deseja obter os dados
+        self.symbol = 'USDBRL'
+
+        # Defina a data de um mês atrás e a data atual
+        self.end_date = datetime.now().strftime('%Y-%m-%d')
+        self.start_date = (datetime.now() - timedelta(days=30)).strftime('%Y-%m-%d')
+
+        # Obtenha os dados históricos da moeda usando a API da Alpha Vantage
+        self.fx = ForeignExchange(key=self.api_key)
+        self.data, _ = self.fx.get_currency_exchange_daily(from_symbol='USD', to_symbol='BRL', outputsize='full')
+        self.data = pd.DataFrame.from_dict(self.data, orient='index').sort_index()
+        self.data = self.data.loc[self.start_date:self.end_date]
+
+        # Calcule a taxa de variação diária da moeda
+        self.daily_returns = self.data['4. close'].astype(float).pct_change()
+
+        # Plote o gráfico da taxa de variação diária
+        plt.plot(self.daily_returns.index, self.daily_returns)
+        plt.title(f'Taxa de variação diária de {self.symbol} em um mês')
+        plt.xlabel('Data')
+        plt.ylabel('Taxa de variação diária')
+        plt.show()       
+        
         
 
 aa = conversor_moeda()
-aa.most()
+aa.graff()
